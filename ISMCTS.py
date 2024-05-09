d862d004b9fcfbe557e73025b98e7ae27659cc03
Lichen Song <lichen.song.personal@gmail.com>
2024-05-09 11:24:47 -0500

    QDTS wip

--- ISMCTS.py
+++ ISMCTS.py
@@ -1,12 +1,19 @@
-from KuhnPoker import InfoSet, Action, HiddenValue, Interval, IntervalLike, KuhnPokerInfoSet, Model, Card, KuhnPokerModel
+from basic_types import Action, HiddenValue, Interval, IntervalLike
+from info_set import InfoSet
+from model import Model
 
 import numpy as np
+
 import abc
-from typing import Dict, List, Optional, Tuple
+from typing import Dict, Optional
 
 
 c_PUCT = 1.0
 DEBUG = True
+
+CHEAT = True  # evaluate model for child nodes immediately, so we don't need Vc
+
+tree_ids = []
 
 def to_interval(i: IntervalLike) -> Interval:
     if isinstance(i, Interval):
@@ -14,12 +21,13 @@ def to_interval(i: IntervalLike) -> Interval:
     return np.array([i, i])
 
 class Node(abc.ABC):
-    def __init__(self, info_set: InfoSet, Q: IntervalLike=0):
+    def __init__(self, info_set: InfoSet, tree_owner: Optional[int] = None, Q: IntervalLike=0):
         self.info_set = info_set
         self.cp = info_set.get_current_player()
         self.game_outcome = info_set.get_game_outcome()
         self.Q: Optional[Interval] = to_interval(Q)
         self.N = 0
+        self.tree_owner = tree_owner
 
     def terminal(self) -> bool:
         return self.game_outcome is not None
@@ -30,33 +38,48 @@ class Node(abc.ABC):
 
 
 class ActionNode(Node):
-    def __init__(self, info_set: InfoSet, initQ: IntervalLike=0):
-        super().__init__(info_set, Q=initQ)
+    def __init__(self, info_set: InfoSet, tree_owner: Optional[int] = None, initQ: IntervalLike=0, spawned_tree=None):
+        super().__init__(info_set, tree_owner=tree_owner, Q=initQ)
 
         self.actions = info_set.get_actions()
         self.children: Dict[Action, Node] = {}
         self.P = None
         self.V = None  #self.game_outcome if self.terminal() else None
         self.Vc = None
+        self.spawned_tree = spawned_tree
+        # self.recent_action = None
 
         if self.terminal():
-            self.Q = to_interval(self.game_outcome[self.cp])
+            self.Q = to_interval(self.game_outcome[self.tree_owner])
 
         self.PURE = np.zeros(len(self.actions))
         self.MIXED = np.zeros(len(self.actions))
         self.n_mixed = 0
         self.n_pure = 0
 
+    def __str__(self):
+        return f'Action({self.info_set}, N={self.N}, Q={self.Q}), V={self.V}'
+
+    def eval_model(self, model: Model):
+        if self.P is not None:
+            # already evaluated
+            return
+        self.P, self.V, self.Vc = model.action_eval(self.tree_owner, self.info_set)
+
     def expand(self, model: Model):
-        self.P, self.V, self.Vc = model.action_eval(self.info_set)
+        if self.terminal():
+            return
+        self.eval_model(model)
         for a in self.actions:
             info_set = self.info_set.apply(a)
             if self.cp != info_set.get_current_player() and info_set.has_hidden_info():
-                self.children[a] = SamplingNode(info_set, self.Vc[a])
+                self.children[a] = SamplingNode(info_set, tree_owner=self.tree_owner, initQ=self.Vc[a])
             else:
-                self.children[a] = ActionNode(info_set, self.Vc[a])
+                self.children[a] = ActionNode(info_set, tree_owner=self.tree_owner, initQ=self.Vc[a])
+            if CHEAT:
+                self.children[a].eval_model(model)
 
-        self.Q = to_interval(self.V[self.cp])
+        self.Q = to_interval(self.V)
 
     def computePUCT(self):
         c = len(self.children)
@@ -69,15 +92,22 @@ class ActionNode(Node):
             Q[i] = child.Q
             N[i] = child.N
 
-        PUCT = Q + c_PUCT * P * np.sqrt(np.sum(N)) / (N + 1)
+        PUCT = c_PUCT * P * np.sqrt(np.sum(N)) / (N + 1)
+        PUCT = Q + PUCT[:, np.newaxis]
 
         # check for pure case
         max_lower_bound_index = np.argmax(PUCT[:, 0])
         max_lower_bound = PUCT[max_lower_bound_index, 0]
-        return Q, np.where(PUCT[:, 1] >= max_lower_bound)[0]
+
+        if DEBUG:
+            print('-- PUCT:')
+            for q, n, puct in zip(Q, N, PUCT):
+                print(f'Q: {q}, N: {n}, PUCT: {puct}')
+
+        return Q, np.where(PUCT[:, 1] >= max_lower_bound - 1e-8)[0]
 
     def get_mixing_distribution(self, action_indices):
-        mask = np.zeros_like(P)
+        mask = np.zeros_like(self.P)
         mask[action_indices] = 1
         P = self.P * mask
 
@@ -86,48 +116,86 @@ class ActionNode(Node):
         return P / s
 
     def visit(self, model: Model):
+        if DEBUG:
+            print(f'= Visiting {self}:')
+
         self.N += 1
 
         if self.terminal():
-            return
+            if DEBUG:
+                print(f'= end visit {self} hit terminal, return Q: {self.Q}')
+            return self.Q
 
         if self.P is None:
             self.expand(model)
-            return
+            if DEBUG:
+                print(f'- expanding {self}')
+                for k, child in self.children.items():
+                    print(f'  - {k}: {child}')
+                print(f'= end visit {self} expand, return self.Q: {self.Q}')
+            return self.Q
 
-        Qc, action_indices = self.computePUCT()
-        if len(action_indices) == 1:  # pure case
-            self.n_pure += 1
-            action_index = action_indices[0]
-            pure_distr = np.zeros(len(self.P))
-            pure_distr[action_index] = 1
-            self.PURE = (self.PURE * (self.n_pure-1) + pure_distr) / self.n_pure
-        else:  # mixed case
-            self.n_mixed += 1
-            mixing_distr = self.get_mixing_distribution(action_indices)
-            action_index = np.random.choice(len(self.P), p=mixing_distr)
-            self.MIXED = (self.MIXED * (self.n_mixed-1) + mixing_distr) / self.n_mixed
+        old_Q = self.Q
+        if self.spawned_tree is not None:
+            action = self.spawned_tree.get_spawned_tree_action()
+            child_Q = self.children[action].visit(model)
 
-        na = np.newaxis
-        E_mixed = np.sum(Qc * self.MIXED[:, na], axis=0)
-        E_pure = np.sum(Qc * self.PURE[:, na], axis=0)
-        self.Q = (self.n_mixed * E_mixed + self.n_pure * E_pure) / (self.n_mixed + self.n_pure)
-        assert self.Q.shape == (2, )
+            self.Q = self.Q * (self.N - 1) / self.N + child_Q / self.N
 
-        action = self.actions[action_index]
-        self.children[action].visit(model)
+            if DEBUG:
+                print(f'- spawned tree action: {action}')
+                print(f'- update Q to {self.Q} from {old_Q}')
+                print(f'= end visit {self}')
+            return self.Q
+
+        else:
+            Qc, action_indices = self.computePUCT()
+            if len(action_indices) == 1:  # pure case
+                self.n_pure += 1
+                action_index = action_indices[0]
+                pure_distr = np.zeros(len(self.P))
+                pure_distr[action_index] = 1
+                self.PURE = (self.PURE * (self.n_pure-1) + pure_distr) / self.n_pure
+                if DEBUG:
+                    print(f'- pure action {self.PURE}, action_index: {action_index}')
+            else:  # mixed case
+                self.n_mixed += 1
+                mixing_distr = self.get_mixing_distribution(action_indices)
+                action_index = np.random.choice(len(self.P), p=mixing_distr)
+                self.MIXED = (self.MIXED * (self.n_mixed-1) + mixing_distr) / self.n_mixed
+                if DEBUG:
+                    print(f'- mixed action {self.MIXED}, action_index: {action_index}')
+
+            na = np.newaxis
+            E_mixed = np.sum(Qc * self.MIXED[:, na], axis=0)
+            E_pure = np.sum(Qc * self.PURE[:, na], axis=0)
+            self.Q = (self.n_mixed * E_mixed + self.n_pure * E_pure) / (self.n_mixed + self.n_pure)
+            assert self.Q.shape == (2, )
+
+            action = self.actions[action_index]
+            self.recent_action = action
+            self.children[action].visit(model)
+
+            if DEBUG:
+                print(f'- update Q to {self.Q} from {old_Q}')
+                print(f'= end visit {self}')
 
 
 class SamplingNode(Node):
-    def __init__(self, info_set: InfoSet, initQ: IntervalLike=0):
-        super().__init__(info_set, Q=initQ)
+    def __init__(self, info_set: InfoSet, tree_owner: Optional[int] = None, initQ: IntervalLike=0, eps=0.01):
+        super().__init__(info_set, tree_owner, Q=initQ)
         self.H = None
         self.V = None
         self.Vc = None
         self.H_mask = info_set.get_H_mask()
         assert np.any(self.H_mask)
         self.children: Dict[HiddenValue, Node] = {}
-    
+        self._expanded = False
+        self.eps = eps
+
+    def __str__(self):
+        return f'Hidden({self.info_set}, N={self.N}, Q={self.Q}), V={self.V}'
+
     def apply_H_mask(self):
         self.H *= self.H_mask
 
@@ -150,9 +218,9 @@ class SamplingNode(Node):
         Computes:
 
         Phi(H) = union_{H' in N_epsilon(H)} phi(H')
-        
-        where 
-        
+
+        where
+
         N_epsilon(H) = {H' | ||H - H'||_1 <= epsilon}
         Q_left = Q[:, 0]
         Q_right = Q[:, 1]
@@ -197,54 +265,120 @@ class SamplingNode(Node):
             print('Phi = %s' % output)
         return output
 
-    def expand(self, model: Model):
-        self.H, self.V, self.Vc = model.hidden_eval(self.info_set)
+    def eval_model(self, model: Model):
+        if self.H is not None:
+            # already evaluated
+            return
+        self.H, self.V, self.Vc = model.hidden_eval(self.tree_owner, self.info_set)
         self.apply_H_mask()
+        self.Q = to_interval(self.V)
+
+    def expand(self, model: Model):
+        self.eval_model(model)
 
         for h in np.where(self.H_mask)[0]:
             info_set = self.info_set.instantiate_hidden_state(h)
             if info_set.has_hidden_info():
                 assert self.cp == info_set.get_current_player()
-                self.children[h] = SamplingNode(info_set, self.Vc[h])
+                self.children[h] = SamplingNode(info_set, tree_owner=self.tree_owner, initQ=self.Vc[h])
             else:
-                self.children[h] = ActionNode(info_set, self.Vc[h])
+                self.children[h] = ActionNode(info_set, tree_owner=self.tree_owner, initQ=self.Vc[h])
+                self.children[h].spawned_tree = self.create_spawned_tree(info_set, model)
+            if CHEAT:
+                self.children[h].eval_model(model)
+            if DEBUG:
+                print(f'  - {h}: {self.children[h]}')
+                if self.children[h].spawned_tree is not None:
+                    print(f'  - spawned tree: {self.children[h].spawned_tree}')
 
-        self.Q = to_interval(self.V[self.cp])
+        self._expanded = True
+
+    def create_spawned_tree(self, info_set: InfoSet, model: Model):
+        info_set = info_set.clone()
+        cp = info_set.get_current_player()
+        for i in range(len(info_set.cards)):
+            if i != cp:
+                info_set.cards[i] = None
+
+        root = ActionNode(info_set)
+        spawned_tree = Tree(model, root)
+        return spawned_tree
+
+    def calc_luck_adjusted_Q(self, Q_h: Interval, h) -> Interval:
+        child_Qs = np.zeros((len(self.children), 2))
+        for i, (_, child) in enumerate(self.children.items()):
+            child_Qs[i] = child.Q
+        child_Qs = np.array(child_Qs)
+
+        ix = list(self.children.keys()).index(h)
+        luck_adjustment = self.Phi(ix, self.eps, child_Qs, self.H[self.H_mask != 0])
+        luck_adjusted_Q = np.array([Q_h[0] - luck_adjustment[1], Q_h[1] - luck_adjustment[0]])
+
+        if DEBUG:
+            print(f'-- calc luck adjusted Q:')
+            for q in child_Qs:
+                print(f'-- child_Qs: {q}')
+            print(f'-- luck_adjustment: {luck_adjustment}')
+
+        return luck_adjusted_Q
+
 
     def visit(self, model: Model):
+        if DEBUG:
+            print(f'= Visiting {self}:')
+
         self.N += 1
 
-        if self.H is None:
+        if not self._expanded:
+            if DEBUG:
+                print(f'- expanding {self}')
             self.expand(model)
 
         h = np.random.choice(len(self.H), p=self.H)
+        if DEBUG:
+            print(f'- sampling hidden state {h} from {self.H}')
 
-        self.children[h].visit(model)
 
-        
+        Q_h = self.children[h].visit(model)
+        luck_adjusted_Q = self.calc_luck_adjusted_Q(Q_h, h)
+
+        old_Q = self.Q
+        self.Q = self.Q * (self.N - 1) / self.N + luck_adjusted_Q / self.N
+        if DEBUG:
+            print(f'- Q_h: {Q_h}, luck_adjusted: {luck_adjusted_Q}')
+            print(f'- update Q to {self.Q} from {old_Q}')
+            print(f'= end visit {self}')
+
+
 class Tree:
     def __init__(self, model: Model, root: ActionNode):
         self.model = model
         self.root = root
+        self.tree_owner = root.info_set.get_current_player()
+        self.root.tree_owner = self.tree_owner
+
+        if id(self) not in tree_ids:
+            tree_ids.append(id(self))
+
+    def __str__(self):
+        return f'Tree(owner={self.tree_owner}, root={self.root})'
 
     def get_visit_distribution(self, n: int) -> Dict[Action, float]:
         while self.root.N <= n:
             if DEBUG:
-                print(f'=============== # visit: {self.root.N}  ===============')
+                print(f'\n=============== visit tree: {tree_ids.index(id(self))}, owner: {self.tree_owner} N: {self.root.N}, root: {self.root} id: {id(self)}  ===============')
             self.root.visit(self.model)
             if self.root.N == 1:
                 continue
 
-        n_total = self.root.N
+        n_total = self.root.N - 1
         return {action: node.N / n_total for action, node in self.root.children.items()}
 
-    def visit(self):
-        self.root.visit()
-
-
-if __name__ == '__main__':
-    info_set = KuhnPokerInfoSet([Action.PASS, Action.ADD_CHIP], [Card.QUEEN, None])
-    model = KuhnPokerModel(1/3, 1/3)
-    root = ActionNode(info_set)
-    mcts = Tree(model, root)
-    mcts.get_visit_distribution(5)
+    def get_spawned_tree_action(self):
+        if DEBUG:
+                print(f'\n=============== get action from spawn tree: {tree_ids.index(id(self))}, owner: {self.tree_owner} N: {self.root.N}, root: {self.root}, id: {id(self)}  ===============')
+        while self.root.N < 1:
+            self.root.visit(self.model)
+        self.root.visit(self.model)
+        action = self.root.recent_action
+        return action
--- KuhnPoker.py
+++ KuhnPoker.py
@@ -1,11 +1,16 @@
-import abc
-from typing import Dict, List, Optional, Tuple
-import numpy as np
-from enum import Enum
+from basic_types import Action, HiddenArray, HiddenValue, PolicyArray, Value, ValueChildArray
+from ISMCTS import ActionNode, Tree
+from info_set import InfoSet
+from model import Model
 
-class Action(Enum):
-    PASS = 0
-    ADD_CHIP = 1
+import numpy as np
+
+from enum import Enum
+from typing import List, Optional, Tuple
+
+
+PASS = 0
+ADD_CHIP = 1
 
 
 class Player(Enum):
@@ -18,59 +23,26 @@ class Card(Enum):
     QUEEN = 1
     KING = 2
 
-Value = float
-Interval = np.ndarray  # shape of (2,)
-IntervalLike = Interval | float
-HiddenValue = int
-
-class InfoSet(abc.ABC):
-    @abc.abstractmethod
-    def has_hidden_info(self) -> bool:
-        pass
-
-    @abc.abstractmethod
-    def get_current_player(self) -> int:
-        pass
-    
-    @abc.abstractmethod
-    def get_game_outcome(self) -> Optional[int]:
-        pass
-    
-    @abc.abstractmethod
-    def get_actions(self) -> List[Action]:
-        pass
-    
-    @abc.abstractmethod
-    def get_H_mask(self) -> np.ndarray:
-        pass
-    
-    @abc.abstractmethod
-    def apply(self, action: Action) -> 'InfoSet':
-        pass
-    
-    @abc.abstractmethod
-    def instantiate_hidden_state(self, h: HiddenValue) -> 'InfoSet':
-        pass
 
 class KuhnPokerInfoSet(InfoSet):
     def __init__(self, action_history: List[Action], cards: List[Optional[Card]]=[None, None]):
         self.action_history = action_history
         self.cards = cards
-    
+
     def __str__(self):
-        hist_str = ''.join(map(str, [a.value for a in self.action_history]))
+        hist_str = ''.join(map(str, [a for a in self.action_history]))
         card_str = ''.join(['?' if c is None else c.name[0] for c in self.cards])
         return f'history=[{hist_str}], cards={card_str}'
 
     def __repr__(self):
         return str(self)
-    
+
     def clone(self) -> 'KuhnPokerInfoSet':
         return KuhnPokerInfoSet(list(self.action_history), list(self.cards))
-    
+
     def get_current_player(self) -> Player:
-        return Player(len(self.action_history) % 2)
-    
+        return Player(len(self.action_history) % 2).value
+
     def get_game_outcome(self) -> Optional[Value]:
         """
         None if game not terminal.
@@ -81,38 +53,38 @@ class KuhnPokerInfoSet(InfoSet):
         if len(self.action_history) < 2:
             return None
 
-        if tuple(self.action_history[-2:]) == (Action.PASS, Action.ADD_CHIP):
+        if tuple(self.action_history[-2:]) == (PASS, ADD_CHIP):
             return None
 
-        if tuple(self.action_history[-2:]) == (Action.ADD_CHIP, Action.PASS):
+        if tuple(self.action_history[-2:]) == (ADD_CHIP, PASS):
             winner = self.get_current_player()
         elif self.cards[Player.ALICE.value].value > self.cards[Player.BOB.value].value:
-            winner = Player.ALICE
+            winner = 0 #Player.ALICE.value``
         else:
-            winner = Player.BOB
+            winner = 1 #Player.BOB.value
 
-        loser = Player(1 - winner.value)
+        loser = Player(1 - winner).value
         loser_pot_contribution = self._get_pot_contribution(loser)
 
         outcome = np.zeros(2)
-        outcome[winner.value] = loser_pot_contribution
-        outcome[loser.value] = -loser_pot_contribution
+        outcome[winner] = loser_pot_contribution
+        outcome[loser] = -loser_pot_contribution
         return outcome
-    
+
     def _get_pot_contribution(self, player: Player):
-        action_values = [a.value for i, a in enumerate(self.action_history) if i % 2 == player.value]
+        action_values = [a for i, a in enumerate(self.action_history) if i % 2 == player]
         return 1 + sum(action_values)
-    
+
     def apply(self, action: Action) -> 'InfoSet':
         action_history = self.action_history + [action]
         cards = list(self.cards)
-        return InfoSet(action_history, cards)
-    
+        return KuhnPokerInfoSet(action_history, cards)
+
     def has_hidden_info(self) -> bool:
         return None in self.cards
-    
+
     def get_actions(self) -> List[Action]:
-        return [a for a in Action]
+        return [PASS, ADD_CHIP]
 
     def get_H_mask(self) -> np.ndarray:
         H_mask = np.zeros(len(Card))
@@ -121,24 +93,15 @@ class KuhnPokerInfoSet(InfoSet):
         ix = [c.value for c in Card if c not in self.cards]
         H_mask[ix] = 1
         return H_mask
-    
+
     def instantiate_hidden_state(self, h: HiddenValue) -> 'InfoSet':
         cp = self.get_current_player()
         cards = list(self.cards)
-        cards[cp.value] = Card(h)
+        cards[cp] = Card(h)
         return KuhnPokerInfoSet(list(self.action_history), cards)
 
-class Model(abc.ABC):
-    # add abstract methods
-    @abc.abstractmethod
-    def action_eval(self, info_set: InfoSet) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
-        pass
-    
-    @abc.abstractmethod
-    def hidden_eval(self, info_set: InfoSet) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
-        pass
 
-class KuhnPokerModel(Model):
+class  KuhnPokerModel(Model):
     def __init__(self, p, q):
         self.p = p
         self.q = q
@@ -155,46 +118,71 @@ class KuhnPokerModel(Model):
 
         self._P_tensor[0, J] = np.array([1-p, p])  # bluff with a Jack with prob p
         self._P_tensor[1, Q] = np.array([1-q, q])  # call with a Queen with prob q
-   
-    def action_eval(self, info_set: InfoSet) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+
+        self._V_tensor = np.zeros((2, 2, 3))  # owner, prev_action, card
+        # self._V_hidden_tensor = np.zeros((2, 2, 3))  # owner, prev_action, card
+
+        self._V_tensor[0, 0, J] = -1
+        self._V_tensor[0, 0, Q] = +1
+        self._V_tensor[0, 0, K] = +1
+
+        self._V_tensor[0, 1, J] = -1
+        self._V_tensor[0, 1, Q] = 2 * (p - 1) / (1 + p)
+        self._V_tensor[0, 1, K] = +2
+
+        self._V_tensor[1, 0, J] = -1 + p * (1 + 3*q) / 2
+        self._V_tensor[1, 0, Q] = 0
+        self._V_tensor[1, 0, K] = 1 + q / 2
+
+        self._V_tensor[1, 1, :] = None  # N/A, we assume Alice checks
+
+        # self._V_hidden_tensor[0, 0] = None  # game has terminated if Bob checks
+
+        # self._V_hidden_tensor[0, 1, J] = -1
+        # self._V_hidden_tensor[0, 1, Q] = 2 * (p - 1) / (1 + p)
+        # self._V_hidden_tensor[0, 1, K] = +2
+
+        # self._V_hidden_tensor[1, 0, J] = -1 + p * (1 + 3*q) / 2
+        # self._V_hidden_tensor[1, 0, Q] = 0
+        # self._V_hidden_tensor[1, 0, K] = 1 + q / 2
+
+    def action_eval(self, tree_owner: int, info_set: InfoSet) -> Tuple[PolicyArray, Value, ValueChildArray]:
         cp = info_set.get_current_player()
-        card = info_set.cards[cp.value]
+        card = info_set.cards[cp]
         assert card is not None
-        x = info_set.action_history[-1].value
+        x = info_set.action_history[-1]
         y = card.value
         P = self._P_tensor[x][y]
-        V = 0
-        Vc = np.zeros(len(Action))
+        V = self._V_tensor[tree_owner][x][y]
+        Vc = np.zeros(2)
 
         return P, V, Vc
-    
-    def hidden_eval(self, info_set: InfoSet) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+
+    def hidden_eval(self, tree_owner: int, info_set: InfoSet) -> Tuple[HiddenArray, Value, ValueChildArray]:
         cp = info_set.get_current_player()
         H = np.ones(3)
         for k in range(len(info_set.action_history) - 1):
-            if k % 2 == cp.value:
+            if k % 2 == cp:
                 continue
-            x = info_set.action_history[k].value
-            y = info_set.action_history[k+1].value
+            x = info_set.action_history[k]
+            y = info_set.action_history[k+1]
             H *= self._P_tensor[x, :, y]
 
-        card = info_set.cards[1 - cp.value]
+        card = info_set.cards[1 - cp]
         assert card is not None
         H[card.value] = 0
         H /= np.sum(H)
 
-        V = 0
-        Vc = np.zeros(len(Action))
+        V = self._V_tensor[tree_owner][x][y]
+        Vc = np.zeros(len(Card))
         return H, V, Vc
 
-if __name__ == '__main__':
-    info_set = KuhnPokerInfoSet([Action.PASS, Action.ADD_CHIP], [None, Card.JACK])
-    print(info_set)
-    print(info_set.get_current_player())
-    print('get_game_outcome: ', info_set.get_game_outcome())
-    print('get_actions: ', info_set.get_actions())
-    print('get_H_mask: ', info_set.get_H_mask())
 
+if __name__ == '__main__':
+    info_set = KuhnPokerInfoSet([PASS, ADD_CHIP], [Card.QUEEN, None])
+    # info_set = KuhnPokerInfoSet([PASS], [None, Card.JACK])
     model = KuhnPokerModel(1/3, 1/3)
-    # print('model action_eval: ', model.action_eval(info_set))
-    print('model hidden_eval: ', model.hidden_eval(info_set))
+    root = ActionNode(info_set)
+    mcts = Tree(model, root)
+    visit_dist = mcts.get_visit_distribution(1000)
+    print(visit_dist)
